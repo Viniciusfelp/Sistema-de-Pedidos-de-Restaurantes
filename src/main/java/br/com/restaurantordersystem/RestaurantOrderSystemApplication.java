293666cca7317e834dc040320d4f8580ee5f156b
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2023-05-24 03:43:49 -0300

    Initial commit

---
+++ src/main/java/br/com/restaurantordersystem/RestaurantOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.restaurantordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restaurantordersystem/RestaurantOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurantordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
